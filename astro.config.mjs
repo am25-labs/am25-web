230c443c0dccf73f4aa96afa266e6a4d11e75a2e
martirale <hola@alemartir.com>
2026-04-02 22:08:59 -0600

    Adjust link styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,44 @@ export default defineConfig({
 
   markdown: {
     rehypePlugins: [
-      [rehypeExternalLinks, { target: "_blank", rel: ["noopener"] }],
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noopener"],
+          protocols: ["http", "https", "mailto"],
+          content: {
+            type: "element",
+            tagName: "svg",
+            properties: {
+              xmlns: "http://www.w3.org/2000/svg",
+              width: "20",
+              height: "20",
+              viewBox: "0 0 24 24",
+              fill: "none",
+              stroke: "#ffff00",
+              strokeWidth: "2",
+              strokeLinecap: "round",
+              strokeLinejoin: "round",
+              class: "inline-block align-middle shrink-0",
+            },
+            children: [
+              {
+                type: "element",
+                tagName: "path",
+                properties: { d: "M7 7h10v10" },
+                children: [],
+              },
+              {
+                type: "element",
+                tagName: "path",
+                properties: { d: "M7 17 17 7" },
+                children: [],
+              },
+            ],
+          },
+        },
+      ],
     ],
   },
 
