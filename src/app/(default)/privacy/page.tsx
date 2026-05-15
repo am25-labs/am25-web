c40c5f377bc8b6a6ade2c70de1ba8b001277e728
martirale <hola@alemartir.com>
2026-05-15 14:12:18 -0600

    Adjust layout

--- src/app/(default)/privacy/page.tsx
+++ src/app/(default)/privacy/page.tsx
@@ -1,7 +1,7 @@
 import ContentRenderer from "@/components/ContentRenderer";
 import { baseMetadata } from "@/lib/metadata";
 import type { Metadata } from "next";
-import { Badge } from "@/components/ui/badge";
+import { RefreshCwIcon } from "lucide-react";
 import GridContainer from "@/components/grids/GridContainer";
 import GridTwo from "@/components/grids/GridTwo";
 import GridFour from "@/components/grids/GridFour";
@@ -33,14 +33,20 @@ export default async function PrivacyPage() {
   const formattedDate = date ? formatDate(date) : null;
 
   return (
-    <GridContainer>
+    <GridContainer className="mb-16">
       <GridTwo className="mb-8">
         <div className="col-span-full">
           <h1 className="text-3xl md:text-4xl font-bold uppercase">
             {pageTitle}
           </h1>
           {formattedDate && (
-            <Badge className="uppercase mt-8">Updated: {formattedDate}</Badge>
+            <span
+              className="mt-6 flex items-center gap-2 text-sm text-muted-foreground"
+              title="Updated At"
+            >
+              <RefreshCwIcon size={16} />
+              {formattedDate}
+            </span>
           )}
         </div>
       </GridTwo>
