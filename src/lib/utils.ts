bf5eeb4bc76de86d3fe0ca7f1417822194c508f3
martirale <hola@alemartir.com>
2026-05-15 01:24:52 -0600

    Add TZ utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export const CMS_TIME_ZONE = "America/El_Salvador";
+
+type FormatDateOptions = {
+  locale?: string;
+  timeZone?: string;
+};
+
+export function formatDate(
+  date: string,
+  { locale = "en-US", timeZone = CMS_TIME_ZONE }: FormatDateOptions = {},
+): string {
+  return new Intl.DateTimeFormat(locale, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone,
+  }).format(new Date(date));
 }
