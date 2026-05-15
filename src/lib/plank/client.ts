7ba4fb07e37cb99a35339c6972df5d0b1b22b084
martirale <hola@alemartir.com>
2026-05-14 23:27:11 -0600

    Remove Astro project

--- src/lib/plank/client.ts
+++
@@ -1,9 +0,0 @@
-import { createPlankClient } from "@plank-cms/client";
-
-const plank = createPlankClient({
-  url: import.meta.env.PLANK_URL,
-  token: import.meta.env.PLANK_TOKEN,
-  defaultLocale: "en",
-});
-
-export default plank;
