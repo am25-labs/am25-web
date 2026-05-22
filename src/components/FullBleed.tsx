99fac8cd6f531f6834599fe711e685584a66f538
martirale <hola@alemartir.com>
2026-05-21 18:13:36 -0600

    Fix full-bleed overflow

--- src/components/FullBleed.tsx
+++ src/components/FullBleed.tsx
@@ -1,6 +1,6 @@
 export default function FullBleed({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative left-1/2 w-screen -translate-x-1/2">
+    <div className="relative left-1/2 w-[100dvw] max-w-[100dvw] -translate-x-1/2">
       {children}
     </div>
   );
